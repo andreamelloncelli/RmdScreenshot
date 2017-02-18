43a24822a259e769a5a911f75eace7c8430a4da9
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-02-18 17:24:49 +0100

    Version 0.1 (linux only)

---
+++ R/take_screenshot.R
@@ -0,0 +1,32 @@
+
+#
+import_screenshot <- function() {
+	rmd_path  <- rstudioapi::getActiveDocumentContext()$path
+	message(image_dir)
+	image_name <- take_screenshot(rmd_path)
+  link <- paste0( "![](", image_name, ")" )
+  ####
+  rstudioapi::insertText(link)
+}
+
+take_screenshot <- function(rmd_path) {
+	if (!all(grepl(pattern = ".*\\.Rmd", rmd_path))) stop("This function works only in a .Rmd buffer")
+	rmd_name <-   gsub(pattern = "\\.Rmd$", "", basename(rmd_path) )
+	image_dir <- 	paste0( dirname(rmd_path), "/", rmd_name, "_files" )
+  system( paste0("mkdir -p ", image_dir) )
+	image_name <- get_image_name(image_dir)
+	command <- paste0(	"import ", image_name )
+	message(command)
+	system_out <-
+		system(
+			command,
+			intern = TRUE
+		)
+	if  ( !is.null( attr(system_out, "status") ) ) {stop("import command failed on file ", image_name)}
+	image_name
+}
+
+get_image_name <- function(dir) {
+	tempfile(pattern =  "image_", tmpdir = dir,fileext = ".png")
+}
+
