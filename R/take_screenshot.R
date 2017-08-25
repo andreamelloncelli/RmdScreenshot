2ffc297ab25c653935a4d2bb44f9b76fe91e4810
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-08-25 12:58:56 +0200

    dirname changed, to a better standard

--- R/take_screenshot.R
+++ R/take_screenshot.R
@@ -1,17 +1,16 @@
 
-#
 import_screenshot <- function() {
 	rmd_path  <- rstudioapi::getActiveDocumentContext()$path
 	image_name <- take_screenshot(rmd_path)
   link <- paste0( "![](", image_name, ")" )
-  ####
+  ##
   rstudioapi::insertText(link)
 }
 
 take_screenshot <- function(rmd_path) {
 	if (!all(grepl(pattern = ".*\\.Rmd", rmd_path))) stop("This function works only in a .Rmd buffer")
 	rmd_name <-   gsub(pattern = "\\.Rmd$", "", basename(rmd_path) )
-	image_dir <- 	paste0( dirname(rmd_path), "/", rmd_name, "_files" )
+	image_dir <- 	paste0( dirname(rmd_path), "/", rmd_name, "_files.d" )
   system( paste0("mkdir -p ", image_dir) )
 	image_name <- get_image_name(image_dir)
 	command <- paste0(	"import ", image_name )
